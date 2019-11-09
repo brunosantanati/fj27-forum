600a614f75cdad521263394ef025413bbc54a4d0
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-11-09 14:40:18 -0300

    Add DTO for topic

---
+++ src/main/java/br/com/alura/forum/controller/dto/output/TopicBriefOutputDto.java
@@ -0,0 +1,82 @@
+package br.com.alura.forum.controller.dto.output;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.model.topic.domain.TopicStatus;
+
+public class TopicBriefOutputDto {
+	
+	private Long id;
+	private String shortDescription;
+	private long secondsSinceLastUpdate;
+	private String ownerName;
+	private String courseName;
+	private String subcategoryName;
+	private String categoryName;
+	private int numberOfResponses;
+	private boolean solved;
+	
+	public TopicBriefOutputDto(Topic topic) {
+		this.id = topic.getId();
+		this.shortDescription = topic.getShortDescription();
+		this.secondsSinceLastUpdate = getSecondsSince(topic.getLastUpdate());
+		this.ownerName = topic.getOwner().getName();
+		this.courseName = topic.getCourse().getName();
+		this.subcategoryName = topic.getCourse().getSubcategory().getName();
+		this.categoryName = topic.getCourse().getCategoryName();
+		this.numberOfResponses = topic.getNumberOfAnswers();
+		this.solved = TopicStatus.SOLVED.equals(topic.getStatus());
+	}
+	
+	private long getSecondsSince(Instant lastUpdate) {
+		return Duration.between(lastUpdate, Instant.now()).get(ChronoUnit.SECONDS);
+	}
+	
+	public static List<TopicBriefOutputDto> listFromTopics(List<Topic> topics){
+		return topics.stream()
+				.map(TopicBriefOutputDto::new)
+				.collect(Collectors.toList());
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public long getSecondsSinceLastUpdate() {
+		return secondsSinceLastUpdate;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public String getSubcategoryName() {
+		return subcategoryName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public int getNumberOfResponses() {
+		return numberOfResponses;
+	}
+
+	public boolean isSolved() {
+		return solved;
+	}
+
+}
