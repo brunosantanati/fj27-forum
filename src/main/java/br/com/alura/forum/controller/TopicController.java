cdd37ffd87a633a1b4b9404f4d1bf7b0e6d568b4
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-11-09 14:44:56 -0300

    Change Topic controller to return a DTO list

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -3,28 +3,29 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.model.Category;
 import br.com.alura.forum.model.Course;
 import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
 
-@Controller
+@RestController
 public class TopicController {
 	
-	@ResponseBody
-	@RequestMapping("/api/topics")
-	public List<Topic> listTopics(){
+	@GetMapping(value = "/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<TopicBriefOutputDto> listTopics(){
+		
 		Category subcategory = new Category("Java", new Category("Programação"));
 		Course course = new Course("Java e JSF", subcategory);
 		User owner = new User("Fulano", "fulano@gmail.com", "123456");
 		Topic topic = new Topic("Problemas com o JSF", "Erro ao fazer conversão da data", owner, course);
 		
 		List<Topic> topics = Arrays.asList(topic, topic, topic);
-		return topics;
+		return TopicBriefOutputDto.listFromTopics(topics);
 	}
 
 }
