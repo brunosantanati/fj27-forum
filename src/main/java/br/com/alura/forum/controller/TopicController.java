06a818d9b448c7ce98c3056df650adf99883cda5
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-11-09 16:29:13 -0300

    Implement to filter topics

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -3,10 +3,12 @@ package br.com.alura.forum.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.input.TopicSearchInputDto;
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.model.topic.domain.Topic;
 import br.com.alura.forum.repository.TopicRepository;
@@ -18,9 +20,10 @@ public class TopicController {
 	private TopicRepository topicRepository;
 	
 	@GetMapping(value = "/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<TopicBriefOutputDto> listTopics(){
+	public List<TopicBriefOutputDto> listTopics(TopicSearchInputDto topicSearch){
 		
-		List<Topic> topics = topicRepository.findAll();
+		Specification<Topic> topicSearchSpecification = topicSearch.build();
+		List<Topic> topics = topicRepository.findAll(topicSearchSpecification);
 		return TopicBriefOutputDto.listFromTopics(topics);
 	}
 
--- src/main/java/br/com/alura/forum/repository/TopicRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicRepository.java
@@ -2,12 +2,13 @@ package br.com.alura.forum.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import br.com.alura.forum.model.topic.domain.Topic;
 
-public interface TopicRepository extends Repository<Topic, Long> {
+public interface TopicRepository extends Repository<Topic, Long>, JpaSpecificationExecutor<Topic> {
 	
 	@Query("select t from Topic t")
 	List<Topic> list();
