1ff4d519e6d89272cc8bf2d546c5d2f93fc7efc4
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-11-16 16:27:35 -0300

    Allow access to public endpoints

--- src/main/java/br/com/alura/forum/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/configuration/SecurityConfiguration.java
@@ -2,7 +2,9 @@ package br.com.alura.forum.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +22,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService)
 			.passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/api/topics/**").permitAll()
+			.anyRequest().authenticated()
+		.and()
+			.formLogin()
+		.and()
+			.httpBasic();
 	}
 
 }
