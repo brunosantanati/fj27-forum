ebb1d054f5d7f0d05a7eeb52ad6cb57a92c3776b
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2019-11-20 14:48:32 -0300

    Create endpoint to authenticate user and send back a JWT token

--- src/main/java/br/com/alura/forum/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/configuration/SecurityConfiguration.java
@@ -1,12 +1,17 @@
 package br.com.alura.forum.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import br.com.alura.forum.security.service.UsersService;
@@ -28,11 +33,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "/api/topics/**").permitAll()
+			.antMatchers("/api/auth/**").permitAll()
 			.anyRequest().authenticated()
 		.and()
-			.formLogin()
+			.cors()
 		.and()
-			.httpBasic();
+			.csrf().disable()
+		.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/**", "/v2/api-docs",
+				"/webjars/**", "/configuration/**", "/swagger-resources/**");
+	}
+	
+	@Override
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 }
---
+++ src/main/java/br/com/alura/forum/controller/dto/output/AuthenticationTokenOutputDto.java
@@ -0,0 +1,21 @@
+package br.com.alura.forum.controller.dto.output;
+
+public class AuthenticationTokenOutputDto {
+	
+	private String tokenType;
+	private String token;
+	
+	public AuthenticationTokenOutputDto(String tokenType, String token) {
+		this.tokenType = tokenType;
+		this.token = token;
+	}
+
+	public String getTokenType() {
+		return tokenType;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+}
