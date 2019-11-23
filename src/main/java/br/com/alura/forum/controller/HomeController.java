aa77fa5d9b133bf6037c21bd651da79705ab66f9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-11-23 15:36:53 -0300

    Show user name in the welcome message

--- src/main/java/br/com/alura/forum/controller/HomeController.java
+++ src/main/java/br/com/alura/forum/controller/HomeController.java
@@ -1,8 +1,12 @@
 package br.com.alura.forum.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.alura.forum.model.User;
 
 @Controller
 public class HomeController {
@@ -10,8 +14,12 @@ public class HomeController {
 	@RequestMapping("/")
 	@ResponseBody
 	public String index() {
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		User user = (User) authentication.getPrincipal();
+		
 		System.out.println("Log do servidor de que foi feita uma requisição para '/'");
-		return "Bem vindo ao Forum da Alura!";
+		return "Bem vindo ao Forum da Alura " + user.getName() + "!";
 	}
 
 }
