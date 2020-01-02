ddf6578802bbe475f1648429adee608aa5a8eaa4
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-01-01 22:10:59 -0300

    Fix import

--- src/test/java/br/com/alura/forum/repository/TopicRepositoryTests.java
+++ src/test/java/br/com/alura/forum/repository/TopicRepositoryTests.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
